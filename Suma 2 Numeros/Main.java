d3aa6b4048be0e57c3276d9b7034835342514fa1
jordi <jordi>
2025-05-12 14:10:38 +0200

    Ejercicio 2

---
+++ Suma 2 Numeros/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Suma de 2 Numeros");
+        
+        System.out.println("Primer Numero:");
+        int scan1 = sc.nextInt();
+        
+        System.out.println("Segundo Numero:");
+        int scan2 = sc.nextInt();
+
+        int resultado = scan1 + scan2;
+        System.out.println("Resultado:");
+        System.out.println(resultado);
+    }
+}
