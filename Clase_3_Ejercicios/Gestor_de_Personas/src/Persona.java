cd5a2bda78efc1c8cf769cc04d26c5077e646bea
jordi <jordi>
2025-05-21 14:29:45 +0200

    small fix

---
+++ Clase_3_Ejercicios/Gestor_de_Personas/src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    
+    private String nombre;
+    private int edad;
+    private String email;
+
+    public Persona(String n, int e, String email) {
+        this.nombre = n;
+        this.edad = e;
+        this.email = email;
+    }
+
+}
