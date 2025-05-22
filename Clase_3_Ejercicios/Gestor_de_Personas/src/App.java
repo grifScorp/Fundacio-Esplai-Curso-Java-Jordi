1b5cb1d0f6d3a4e5586c60a50b2bc89986096b6f
jordi <jordi>
2025-05-22 13:54:42 +0200

    progress

--- Clase_3_Ejercicios/Gestor_de_Personas/src/App.java
+++ Clase_3_Ejercicios/Gestor_de_Personas/src/App.java
@@ -1,5 +1,91 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class App {
+
+    static ArrayList<Persona> listaPersonas = new ArrayList<>();
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Persona p1 = new Persona("Aurelio", 16, "aurelio@gmail.com");
+        Persona p2 = new Persona("Ezequiel", 20, "Ezequiel@gmail.com");
+
+        listaPersonas.add(p1);
+        listaPersonas.add(p2);
+
+        Scanner sc = new Scanner(System.in);        
+        int opcion = -1;
+
+        while(opcion != 0) {
+
+            System.out.println("\nGestor de Personas\n");
+
+            test();
+            
+            System.out.println("");
+            System.out.println("1. Crear Persona");
+            System.out.println("2. Comprobar Edad");
+            System.out.println("3. Borrar Persona");
+            System.out.println("0. Salir");
+
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+
+            switch (opcion) {
+                case 0:
+                    opcion = 0;
+                    break;
+                case 1:                    
+                    System.out.print("Nombre de la Persona: ");
+                    String nombre = sc.nextLine();
+
+                    System.out.print("Edad: ");
+                    int edad = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Email: ");
+                    String email = sc.nextLine();
+
+                    Persona p3 = new Persona(nombre, edad, email);
+                    
+                    while (!p3.emailArroba()) {
+                        System.out.println("Email Incorrecto, Corrija ");
+                        System.out.println("Email: ");
+                        email = sc.nextLine();
+                        p3.setEmail(email);
+                    }
+                    listaPersonas.add(p3);
+                    System.out.println("Creación Exitosa!\n");                    
+
+                    break;
+                case 2:
+                    System.out.print("Posicion en la lista: ");
+                    int i = sc.nextInt();
+                    sc.nextLine();
+                    Persona p = listaPersonas.get(i);
+
+                    if (p.esMayorEdad()) {
+                        System.out.print(p.getNombre() + " Edad: " + p.getEdad() + " ");
+                        System.out.print("Si es Mayor de Edad");
+                    } else {
+                        System.out.print(p.getNombre() + " Edad: " + p.getEdad() + " ");
+                        System.out.print("No es Mayor de Edad");
+                    }
+                    
+                    break;
+                default:
+                    System.out.println("Opcion no Permitida");;
+                    break;
+            }
+        }
+        sc.close();
+    }
+
+    public static void test() {
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona);
+        }
+        
     }
 }
--- Clase_3_Ejercicios/Gestor_de_Personas/src/Persona.java
+++ Clase_3_Ejercicios/Gestor_de_Personas/src/Persona.java
@@ -10,4 +10,44 @@ public class Persona {
         this.email = email;
     }
 
+    //Getters
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Metodos
+    public boolean esMayorEdad() {
+        return this.edad >= 18; 
+    }
+
+    public boolean emailArroba() {
+        return this.email.contains("@");
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + this.nombre + " Edad: " + this.edad + " Correo: " + this.email;
+    }
+
 }
